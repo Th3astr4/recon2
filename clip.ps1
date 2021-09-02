0d6b4070589b73b6586807d467344f0bf32be4d8
Th3astr4 <87044657+Th3astr4@users.noreply.github.com>
2021-09-02 20:14:48 -0300

    Add files via upload

---
+++ clip.ps1
@@ -0,0 +1,37 @@
+﻿$pclip = ""
+ $array = @()
+ $counter = 0
+
+
+
+    while($true){
+
+$cclip = Get-Clipboard
+
+
+        if ($pclip -eq $cclip){
+
+        #Do Nothing
+        
+        } else {
+
+
+        $array += $cclip
+        $pclip = $cclip
+        $cclip = Get-Clipboard
+
+
+        $counter++
+
+            
+
+            $pclip >> C:\Temp\clp2.txt
+
+            
+
+        }
+     
+        Start-Sleep -Seconds 5
+       
+        }
+        
